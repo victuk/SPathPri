720a6999e567a408738d0508765df5dca76b6106
Victor Ukok <ukokjnr@gmail.com>
2025-03-24 13:19:22 +0100

    Fix: Generate PDF.

--- controllers/generatePDFController.ts
+++ controllers/generatePDFController.ts
@@ -23,7 +23,10 @@ export const generatePDF = async (
     const fileName = v4() + '.pdf';
 
       console.log("htmlContent", htmlContent);
-      const browser = await puppeteer.launch();
+      // const browser = await puppeteer.launch();
+      const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+      });
       const page = await browser.newPage();
       await page.setContent(htmlContent);
       const pdfBuffer = await page.pdf();
