05a49a5af150017f1bd1d17374e152e1799624a6
Victor Ukok <ukokjnr@gmail.com>
2026-07-14 10:39:47 +0100

    Fix: Missing dependencies.

--- utils/redisClientUtil.ts
+++ utils/redisClientUtil.ts
@@ -1,6 +1,6 @@
 import Redis from 'ioredis';
 
-const redisClient = new Redis();
+const redisClient = new Redis(process.env.REDIS_URL || 'redis://127.0.0.1:6379');
 
 redisClient.on('connect', () => {
     console.log('Connected to Redis!');
